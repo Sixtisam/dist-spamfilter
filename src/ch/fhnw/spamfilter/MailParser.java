3e32ad5826a0248e3e5123e113cce689f0140c0e
Stefan Landös <stefan.landoes@students.fhnw.ch>
2019-10-07 13:59:09 +0200

    Changed Mail Parser and File Collector

--- src/ch/fhnw/spamfilter/MailParser.java
+++ src/ch/fhnw/spamfilter/MailParser.java
@@ -1,9 +1,24 @@
 package ch.fhnw.spamfilter;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
+import java.io.File;
 
 public class MailParser {
-	public Set<String> parseMail(String mail) {
-		return null;
+	public static Set<String> parseMail(File mail) {
+	    Set<String> words = new HashSet<>();
+        try {
+            Scanner in = new Scanner(mail);
+            while(in.hasNextLine()){
+                String line = in.nextLine();
+                words.addAll(Arrays.asList(line.split("\\s+")));
+            }
+            in.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return words;
 	}
 }
--- src/ch/fhnw/spamfilter/io/FileCollector.java
+++ src/ch/fhnw/spamfilter/io/FileCollector.java
@@ -1,16 +1,30 @@
 package ch.fhnw.spamfilter.io;
 
 import java.io.File;
-import java.util.List;
+import java.util.ArrayList;
 
 public class FileCollector {
-	// TODO stefan collect all files
+    final static File FOLDER_HAM = new File("ham-anlern");
+    final static File FOLDER_SPAM = new File("spam-anlern");
 
-	public List<File> collectHamLearningMails() {
-		return null;
-	}
-
-	public List<File> collectSpamLearningMails() {
-		return null;
-	}
+    static ArrayList<File> collectMails(File folder) {
+        ArrayList<File> files_list = new ArrayList<File>();
+        for (File fileEntry : folder.listFiles()) {
+            if (fileEntry.isDirectory()) {
+                collectMails(fileEntry);
+            } else {
+                files_list.add(fileEntry);
+                
+            }
+        }
+        return files_list;
+    }
+    
+    public static ArrayList<File> collectHamLearningMails() {
+        return collectMails(FOLDER_HAM);
+    }
+    public static ArrayList<File> collectSpamLearningMails() {
+        return collectMails(FOLDER_SPAM);
+    }
+    
 }
